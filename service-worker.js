3b3c6471e32f059b7c959c1bafe829f27b4b78f7
zube23 <dinozubcicc@gmail.com>
2025-08-10 14:06:16 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('ravnice-cache-v1').then(function(cache) {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icon-512.png'
+      ]);
+    })
+  );
+});
+self.addEventListener('fetch', function(event) {
+  event.respondWith(caches.match(event.request).then(function(response) { return response || fetch(event.request); }));
+});
